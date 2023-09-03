9dcbf49602d83388ec94a2b0a899991684cf8560
Nachop51 <nachoperalta0@gmail.com>
2023-09-02 23:19:53 -0300

    basics

---
+++ tutorial/src/routes/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Nested from "./Nested.svelte";
+</script>
+
+<p>This is a paragraph.</p>
+<Nested />
+
+<!-- Notice that even though Nested.svelte has a <p> element,
+   the styles from App.svelte don't leak in. -->
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: "Comic Sans MS", cursive;
+		font-size: 2em;
+	}
+</style>
---
+++ tutorial/src/routes/Arrays.svelte
@@ -0,0 +1,20 @@
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		// Array.push() will mutate the array, but Svelte will not detect the change
+		numbers = [...numbers, numbers.length + 1];
+	}
+
+	$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+</script>
+
+<p>{numbers.join(" + ")} = {sum}</p>
+
+<button on:click={addNumber}> Add a number </button>
+
+<!-- A simple rule of thumb: the name of the updated variable must appear on the left hand side of the assignment. For example this...
+
+const foo = obj.foo;
+foo.bar = 'baz';
+...won't trigger reactivity on obj.foo.bar, unless you follow it up with obj = obj. -->
