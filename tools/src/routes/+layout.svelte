1056236ceadc75c5aa5ad308f88f85d2d1b80360
Nachop51 <nachoperalta0@gmail.com>
2023-09-09 22:46:41 -0300

    first commit

---
+++ tools/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="flex space-around items-center p-4 bg-slate-50">
+	<a href="/tools">Tools</a>
+	<a href="/">Home</a>
+</nav>
+
+<main class="min-h-screen">
+	<slot />
+</main>
+
+<style>
+	nav a {
+		text-decoration: none;
+		color: #333;
+	}
+</style>
