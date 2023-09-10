dd5b4f8443204458f52c4f3973d278db133c2a6d
Nachop51 <nachoperalta0@gmail.com>
2023-09-10 01:57:17 -0300

    Initializing daisy ui

--- tools/src/routes/+layout.svelte
+++ tools/src/routes/+layout.svelte
@@ -2,18 +2,27 @@
 	import '../app.css';
 </script>
 
-<nav class="flex space-around items-center p-4 bg-slate-50">
-	<a href="/tools">Tools</a>
-	<a href="/">Home</a>
+<nav class="navbar bg-base">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost normal-case text-xl">Home</a>
+	</div>
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/tools">Tools</a></li>
+			<li>
+				<details class="w-full">
+					<summary>List of tools</summary>
+					<ul class="p-2 bg-base-100">
+						<li><a href="/tools/hex">Hex</a></li>
+						<li><a href="/tools/str">Str</a></li>
+						<li><a href="/tools/test">Test</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
 </nav>
 
-<main class="min-h-screen">
+<div class="min-h-screen flex flex-col bg-base mx-auto text-base-content">
 	<slot />
-</main>
-
-<style>
-	nav a {
-		text-decoration: none;
-		color: #333;
-	}
-</style>
+</div>
---
+++ tools/src/routes/tools/hex/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<h1>Hex page</h1>
