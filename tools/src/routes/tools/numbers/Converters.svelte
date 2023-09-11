d718333f67752b00290216b9aa4d46a1bab1f98e
Nachop51 <nachoperalta0@gmail.com>
2023-09-10 23:59:29 -0300

    Functional converter

--- tools/src/routes/tools/numbers/Converters.svelte
+++ tools/src/routes/tools/numbers/Converters.svelte
@@ -1,6 +1,8 @@
 <script>
 	let number = '';
 	$: console.log(number);
+	$: hexNumber = number ? parseInt(number, 10).toString(16) : '';
+	$: octNumber = number ? parseInt(number, 10).toString(8) : '';
 </script>
 
 <form>
@@ -9,15 +11,18 @@
 		placeholder="Type here"
 		class="input input-bordered w-full max-w-xs rounded-xl"
 		bind:value={number}
+		on:input={() => console.log('number', number)}
 	/>
 	<input
 		type="text"
 		placeholder="Type here"
 		class="input input-bordered w-full max-w-xs rounded-xl"
+		bind:value={hexNumber}
 	/>
 	<input
 		type="text"
 		placeholder="Type here"
 		class="input input-bordered w-full max-w-xs rounded-xl"
+		bind:value={octNumber}
 	/>
 </form>
