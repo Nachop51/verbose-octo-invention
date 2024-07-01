83235964c8f77e7e7bb36005e03b9ad58ed52c0d
Nachop51 <nachoperalta0@gmail.com>
2024-07-01 00:56:13 -0300

    cool animotion stuff

---
+++ animotion/src/routes/test/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Transition, Presentation, Slide } from '@animotion/core'
+</script>
+
+<Presentation options={{ controls: false, progress: false, transition: 'slide' }}>
+	<Slide class="h-full place-content-center place-items-center">
+		<Transition>
+			<h1>Hello</h1>
+		</Transition>
+		<Transition>
+			<h1>World</h1>
+		</Transition>
+	</Slide>
+
+	<Slide class="h-full place-content-center place-items-center">wohooo</Slide>
+</Presentation>
